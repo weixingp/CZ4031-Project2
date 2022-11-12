fb3f81a240792aa37d5aa0ff9ead3139224fc4ee
Gabriel <glow015@e.ntu.edu.sg>
2022-11-12 16:21:55 +0800

    Layout optimisations

--- interface.py
+++ interface.py
@@ -133,39 +133,52 @@ def change():
 
 def mainpage(root):
     global textinput, annotate_submit_btn
-    title = Label(root, text="SQL Query Annotator", height=2, bg="#3B86A7", fg="white", font="Inter 48")
-    title.place(anchor=CENTER, relx=0.5, rely=0.12)
-    title.grid(row=0, column=0, columnspan=5, sticky='ew')
-    subtitle = Label(root, text="Enter Query Here:", bg="white", fg="black", font="Inter 18 bold")
-    subtitle.place(x=80, y=140)
+    frame_main_title = Frame(root, bg='#3B86A7', height=60)
+    frame_main_title.pack(fill='x')
+    title = Label(frame_main_title, text="SQL Query Annotator", height=2, bg="#3B86A7", fg="white", font="Inter 48")
+    title.pack(fill='both')
 
-    textinput = Text(width=60, height=20, bg="#CCE4EB")
-    textinput.place(x=80, y=180)
+    frame_main_bottom = Frame(root, bg='white', padx=10, pady=10, width=850, height=490)
+    frame_main_bottom.pack(fill='x')
+    frame_main_bottom.rowconfigure(0, weight=1)
+    frame_main_bottom.columnconfigure(0, weight=3)
+    frame_main_bottom.columnconfigure(1, weight=1)
 
-    annotate_submit_btn = Button(root, text="Annotate", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
-                                 command=getinput)
-    annotate_submit_btn.place(x=80, y=480)
+    frame_main_left = Frame(frame_main_bottom, bg='white', padx=10, pady=10)
+    frame_main_left.grid(row=0, column=0, sticky='nsew')
+
+    frame_main_right = Frame(frame_main_bottom, bg='white', padx=10, pady=10)
+    frame_main_right.grid(row=0, column=1, sticky='nsew')
+
+    subtitle = Label(frame_main_left, text="Enter Query Here:", bg="white", fg="black", font="Inter 18 bold", anchor='w')
+    subtitle.pack(fill='x')
+    textinput = Text(frame_main_left, width=60, height=20, bg="#CCE4EB")
+    textinput.pack(fill='both')
+    Label(frame_main_left, text=" ", bg="white").pack()
+    annotate_submit_btn = Button(frame_main_left, text="Annotate", width=10, height=1, bg="#3B86A7",
+                                 fg="black", font="Inter 16", command=getinput)
+    annotate_submit_btn.pack()
 
     # Example SQL input
 
-    subtitle2 = Label(root, text="Example SQLs:", bg="white", fg="black", font="Inter 18 bold")
-    subtitle2.place(x=550, y=140)
+    subtitle2 = Label(frame_main_right, text="Example SQLs:", bg="white", fg="black", font="Inter 18 bold")
+    subtitle2.pack()
+    Label(frame_main_right, text=" ", bg="white").pack()
 
-    sql1_btn = Button(root, text=f"SQL Query 1", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
-                      command=lambda: insert_sql(0))
-    sql1_btn.place(x=550, y=180 + 40 * 0)
+    Button(frame_main_right, text=f"SQL Query 1", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
+                      command=lambda: insert_sql(0)).pack()
+    Label(frame_main_right, text=" ", bg="white").pack()
 
-    sql2_btn = Button(root, text=f"SQL Query 2", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
-                      command=lambda: insert_sql(1))
-    sql2_btn.place(x=550, y=180 + 40 * 1)
+    Button(frame_main_right, text=f"SQL Query 2", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
+                      command=lambda: insert_sql(1)).pack()
+    Label(frame_main_right, text=" ", bg="white").pack()
 
-    sql3_btn = Button(root, text=f"SQL Query 3", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
-                      command=lambda: insert_sql(2))
-    sql3_btn.place(x=550, y=180 + 40 * 2)
+    Button(frame_main_right, text=f"SQL Query 3", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
+                      command=lambda: insert_sql(2)).pack()
+    Label(frame_main_right, text=" ", bg="white").pack()
 
-    sql4_btn = Button(root, text=f"SQL Query 4", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
-                      command=lambda: insert_sql(3))
-    sql4_btn.place(x=550, y=180 + 40 * 3)
+    Button(frame_main_right, text=f"SQL Query 4", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 16",
+                      command=lambda: insert_sql(3)).pack()
 
 
 def insert_sql(index):
@@ -176,18 +189,30 @@ def insert_sql(index):
 
 
 def outputpage(root, plan_root: PlanNode, no_annotation=False):
-    title = Label(root, text="Query Annotation", bg="#3B86A7", fg="white", font="Inter 48", height=2)
-    title.place(anchor=CENTER, relx=0.5, rely=0.12)
-    title.grid(row=0, column=0, columnspan=5, sticky='ew')
-
-    home_button = Button(root, text="Back", width=5, height=1, bg="#3B86A7", fg="black", font="Inter 16",
+    frame_output_title = Frame(root, bg='#3B86A7', height=60, padx=40)
+    frame_output_title.pack(fill='x')
+    home_button = Button(frame_output_title, text="Back", width=5, height=1, bg="#3B86A7", fg="black", font="Inter 16",
                          command=change)
-    home_button.place(x=25, y=35)
+    home_button.pack(side=LEFT)
 
-    frame_output = Frame(root, bg="white", height=400, width=850, padx=0, pady=0)
-    frame_output.place(x=0, y=100)
+    title = Label(frame_output_title, text="Query Annotation", height=2, bg="#3B86A7", fg="white", font="Inter 48")
+    title.pack(side=LEFT, fill='x', expand=1)
+    Label(frame_output_title, text="Back", width=5, height=1, bg="#3B86A7", fg="#3B86A7",
+          font="Inter 16").pack(side=LEFT)  # padding
 
-    canvas = Canvas(frame_output, bg="white", height=380, width=830)
+    frame_output_content = Frame(root, bg="white", height=490, width=850, padx=0, pady=0)
+    frame_output_content.pack(fill='x')
+    frame_output_content.rowconfigure(0, weight=1)
+    frame_output_content.columnconfigure(0, weight=3)
+    frame_output_content.columnconfigure(1, weight=1)
+
+    frame_output = Frame(frame_output_content, bg="white", height=320, width=850, padx=0, pady=0)
+    frame_output.pack(fill='x')
+
+    frame_output_bottom = Frame(frame_output_content, bg="white", height=170, width=850, padx=0, pady=0)
+    frame_output_bottom.pack(fill='x')
+
+    canvas = Canvas(frame_output, bg="white", height=320, width=830)
     vsb = Scrollbar(frame_output, orient="vertical", command=canvas.yview)
     hsb = Scrollbar(frame_output, orient="horizontal", command=canvas.xview)
     canvas.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
@@ -206,19 +231,28 @@ def outputpage(root, plan_root: PlanNode, no_annotation=False):
     canvas.create_window((415, 250), window=scrollable_frame, anchor="n")
     draw_tree(plan_root, scrollable_frame, no_annotation)
 
-    plan_cost_title = Label(root, text=f"Plan cost: {plan_root.cost}", bg="white", fg="black", font="Inter 18 bold")
-    plan_cost_title.place(x=20, y=120)
+    plan_cost_title = Label(frame_output_content, text=f"Plan cost: {plan_root.cost}", bg="white", fg="black",
+                            font="Inter 18 bold")
+    plan_cost_title.place(x=20, y=20)
 
     Label(scrollable_frame, text="", bg="white").pack()
 
     # bottom page
-    original_query_box = Text(width=60, height=10, bg="#CCE4EB")
-    original_query_box.place(x=30, y=505)
+    original_query_box = Text(frame_output_bottom, width=60, height=10, bg="#CCE4EB")
+    original_query_box.pack(side=LEFT)
     original_query_box.insert("1.0", query)
 
-    plan_cost_title = Label(root, text=f"Alt Plans:", bg="white", fg="black", font="Inter 16")
-    plan_cost_title.place(x=480, y=505)
-    alt_plans_btns()
+    frame_output_bottom_aqp = Frame(frame_output_bottom, bg="white", width=200, padx=20)
+    frame_output_bottom_aqp.pack(side=LEFT, fill='both')
+
+    frame_output_bottom_qep = Frame(frame_output_bottom, bg="white")
+    frame_output_bottom_qep.pack(side=LEFT, fill='both', expand=1)
+
+    plan_cost_title = Label(frame_output_bottom_aqp, text=f"Alt Plans:", bg="white", fg="black", font="Inter 16",
+                            anchor='w')
+    plan_cost_title.pack()
+    alt_plans_btns(frame_output_bottom_aqp, frame_output_bottom_qep)
+
 
 
 def refreshOutputpage(root, plan_root: PlanNode, no_annotation=False):
@@ -227,25 +261,16 @@ def refreshOutputpage(root, plan_root: PlanNode, no_annotation=False):
     outputpage(root, plan_root, no_annotation)
 
 
-def alt_plans_btns():
-    org_btn = Button(root, text=f"Original Plan", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 14",
+def alt_plans_btns(frame_aqp, frame_qep):
+    org_btn = Button(frame_qep, text=f"Original Plan", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 14",
                      command=partial(refreshOutputpage, root, qp.qep, False))
-    org_btn.place(x=700, y=505)
+    org_btn.place(relx=0.5, rely=0.5, anchor=CENTER)
     keep = []
-    i = 0
-    top_padding = 0
-    right_padding = 0
     for key, node in qp.aqp.items():
-        plan1_btn = Button(root, text=f"No {key}", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 14",
+        plan1_btn = Button(frame_aqp, text=f"No {key}", width=10, height=1, bg="#3B86A7", fg="black", font="Inter 14",
                            command=partial(refreshOutputpage, root, node, True))
-        plan1_btn.place(x=480 + right_padding, y=540 + top_padding)
+        plan1_btn.pack()
         keep.append(plan1_btn)
-        i += 1
-        top_padding += 40
-        if i >= 2:
-            top_padding = 0
-            right_padding += 150
-
 
 def connect_db_form():
     global top
@@ -348,7 +373,6 @@ root.geometry("850x650")
 root.resizable(False, False)
 root.title("CZ4031 Database System Principles GUI")
 root.configure(background="white")
-root.columnconfigure(0, weight=1)
 center(root)
 connect_db_form()
 root.mainloop()
